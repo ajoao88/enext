7a52a88a814c130e45f44b61c9064cc1fad1bac8
João Paulo <a.joao1988@gmail.com>
2019-11-21 23:10:31 -0300

    Corrigir indentação do código
    Sempre indentar com dois espaços, normalmente os editores já tem o tab configurado
    para dar 2 espaços

--- js/dog.js
+++ js/dog.js
@@ -1,65 +1,64 @@
-$("#consultar").click(function(){
-	console.log("click exibe a imagem");
-	$.get( "https://dog.ceo/api/breeds/image/random", function(data) {
+$("#consultar").click(function () {
+  console.log("click exibe a imagem");
+  $.get("https://dog.ceo/api/breeds/image/random", function (data) {
 
-            var output="<ul>";
-            for (var i in data.Product.ProductImage) 
-            {
-                output+="<li>" + data.Product.ProductImage.i.http  + "</li>";
-            }
-            output+="</ul>";
+    var output = "<ul>";
+    for (var i in data.Product.ProductImage) {
+      output += "<li>" + data.Product.ProductImage.i.http + "</li>";
+    }
+    output += "</ul>";
 
-            $('span').html(output);
-	});
+    $('span').html(output);
+  });
 
-$('#consultar').html(dadosjSon);
+  $('#consultar').html(dadosjSon);
 });
 
 function getDog() {
-    var selectedDog = $(".dog-selector option:selected").val();
-    dogURL = selectedDog.replace(/-/g, '/');
-    $.getJSON("https://dog.ceo/api/breed/" + dogURL + "/images/random", function(result) {
-        $(".images-dog").html("<img src='" + result.message + "'>");
-    });
+  var selectedDog = $(".dog-selector option:selected").val();
+  dogURL = selectedDog.replace(/-/g, '/');
+  $.getJSON("https://dog.ceo/api/breed/" + dogURL + "/images/random", function (result) {
+    $(".images-dog").html("<img src='" + result.message + "'>");
+  });
 }
 
 function loadDogs() {
-    $.getJSON("https://dog.ceo/api/breeds/list/all", function(result) {
-        var breeds = result.message;
-        firstDog = Object.keys(breeds)[0];
-        $.each(breeds, function(dog, breed) {
-            if (breeds[dog].length >= 1) {
-                for (i = 0; i < breeds[dog].length; i++) {
-                    $(".dog-selector").append('<option value="' + dog + '-' + breeds[dog][i] + '">' + breeds[dog][i] + ' ' + dog + '</option>');
-                }
-            } else if (breeds[dog].length < 1) {
-                $(".dog-selector").append('<option value="' + dog + '">' + dog + '</option>');
-            }
-        });
-        $.getJSON("https://dog.ceo/api/breed/" + firstDog + "/images/random", function(result) {
-            $(".images-dog").html("<img src='" + result.message + "'>");
-        });
+  $.getJSON("https://dog.ceo/api/breeds/list/all", function (result) {
+    var breeds = result.message;
+    firstDog = Object.keys(breeds)[0];
+    $.each(breeds, function (dog, breed) {
+      if (breeds[dog].length >= 1) {
+        for (i = 0; i < breeds[dog].length; i++) {
+          $(".dog-selector").append('<option value="' + dog + '-' + breeds[dog][i] + '">' + breeds[dog][i] + ' ' + dog + '</option>');
+        }
+      } else if (breeds[dog].length < 1) {
+        $(".dog-selector").append('<option value="' + dog + '">' + dog + '</option>');
+      }
     });
+    $.getJSON("https://dog.ceo/api/breed/" + firstDog + "/images/random", function (result) {
+      $(".images-dog").html("<img src='" + result.message + "'>");
+    });
+  });
 }
-$(".dog-selector").change(function() {
-    $(".dog-selector option:selected").each(function() {
-        getDog();
-    });
-});
-$(".get-dog").click(function() {
+$(".dog-selector").change(function () {
+  $(".dog-selector option:selected").each(function () {
     getDog();
+  });
 });
-$(document).ready(function() {
-    loadDogs();
+$(".get-dog").click(function () {
+  getDog();
 });
-function gravarDadosSession(){
-	localStorage.setItem('nomeCachorro',nomeCachorro.value);
+$(document).ready(function () {
+  loadDogs();
+});
+function gravarDadosSession() {
+  localStorage.setItem('nomeCachorro', nomeCachorro.value);
 
 }
-function salvaNome(){
+function salvaNome() {
 
-	var nome = localStorage.getItem('nomeCachorro');
-	
-  	document.getElementById('status').innerHTML=nome+"<br>"+email;
-	document.getElementById('avatar').innerHTML= "<img src="+imagem+" />";
+  var nome = localStorage.getItem('nomeCachorro');
+
+  document.getElementById('status').innerHTML = nome + "<br>" + email;
+  document.getElementById('avatar').innerHTML = "<img src=" + imagem + " />";
 }
